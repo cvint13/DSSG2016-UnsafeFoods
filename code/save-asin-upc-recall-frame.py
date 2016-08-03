248891268d7bac4b3daf27a076d219ce485ba6b8
Cynthia Vint <cvint13@live.com>
2016-08-03 19:19:03 -0400

    amazon reviews insert script- not to be used

--- code/save-asin-upc-recall-frame.py
+++ code/save-asin-upc-recall-frame.py
@@ -2,23 +2,25 @@ from urllib.request import urlopen
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 import re
+
+import sys
+import os
+sys.path.append('.')
+
+print(os.getcwd())
+import data_preprocessing as dp
+
 import pandas
-recall_data = pandas.read_csv("recalls_upcs_asins_joined.csv",skip_blank_lines = True,encoding='ISO-8859-1')
+recall_data = pandas.read_csv("data/enforce_upcs.csv")
 
-run data-preprocessing.py
-
-count = 0
-print(count)
 for row in range(recall_data.shape[0]):
-    upcs = str(recall_data.upcs[row]).split(';')
+    upcs = str(recall_data.upc[row]).split(';')
     asins = []
     upcs = list(set(upcs))
     for upc in upcs:
         print(upc)
         upc = str(upc).strip()
-        asins.append(UPCtoASIN(upc))
+        asins.append(dp.UPCtoASIN(upc))
     recall_data.asins[row] = asins
-    print(count)
-    count+=1
 	
-recall_data.to_csv('recalls_upcs_asins_joined.csv')
+recall_data.to_csv('data/recalls_upcs_asins_joined.csv')
--- sql/amazon_reviews_insert_script.py
+++ sql/amazon_reviews_insert_script.py
@@ -7,6 +7,7 @@ Created on Thu Jul  7 18:32:15 2016
 
 import psycopg2
 import pandas
+import numpy as np
 from datetime import datetime
 
 #Connect to database
@@ -30,10 +31,21 @@ reviewer_ids = pandas.DataFrame(cur.fetchall())
 reviewer_ids = list(reviewer_ids.iloc[:,0])
 print(reviewer_ids[:10])
 
+#get dataframe of all reviewers and their review times
+cur.execute('SELECT reviewer.amazon_reviewer_id, review.unix_review_time\
+            from reviewer join review on reviewer.reviewer_id = review.reviewer_id')
+            
+reviewer_data = pandas.DataFrame(cur.fetchall())
+
 count = 0
 
-for row in range(review_data.shape[0]):
+val_string = ''
+
+reviewer_string = ''
+
+for row in range(246850+68300+159800+140100+48800+9900+17600+2600+21500,review_data.shape[0]):
     count += 1
+    print(count)
     review_date = review_data.reviewTime[row]
     review_text = review_data.reviewText[row]
     summary = review_data.summary[row]
@@ -47,28 +59,38 @@ for row in range(review_data.shape[0]):
     reviewer_name = str(review_data.reviewerName[row]).replace('\'', '')
 	#check to see if reviewer already exists
     if amazon_reviewer_fk not in reviewer_ids:
-        cur.execute('INSERT INTO REVIEWER (reviewer_id, amazon_reviewer_id, reviewer_name)\
-        VALUES (nextval(\'reviewer_serial\'),  \'%s\', \'%s\');' % (amazon_reviewer_fk, reviewer_name))
+        reviewer_string+= ', (nextval(\'reviewer_serial\'),  \'%s\', \'%s\')' % (amazon_reviewer_fk, reviewer_name)
         reviewer_ids.append(amazon_reviewer_fk)
 	
     #check if review already exists by checking unix review time and amazon reviewer id
-    cur.execute('Select * from review where unix_review_time = %d and reviewer_id in (select reviewer_id from reviewer\
-				where amazon_reviewer_id = \'%s\');' % (unix_review_time, amazon_reviewer_fk))
+    #cur.execute('Select * from review where unix_review_time = %d and reviewer_id in (select reviewer_id from reviewer\
+    #				where amazon_reviewer_id = \'%s\');' % (unix_review_time, amazon_reviewer_fk))
+    exists = False
+    reviewer_rows = reviewer_data[reviewer_data[0] == amazon_reviewer_fk].index.tolist()
+    for rrow in reviewer_rows:
+        if reviewer_data.iloc[rrow,1] == int(unix_review_time):
+            exists = True
+            print(reviewer_data.iloc[rrow,1])
+            break
 	
-    result = pandas.DataFrame(cur.fetchall())
-    print(result.shape)
-	
-    if result.shape[0] == 0:
-        cur.execute('INSERT INTO Review (review_id, reviewer_id, product_id, review_text, \
-        summary, overall, unix_review_time, review_time) VALUES (nextval(\'review_serial\'), \
-        (SELECT reviewer_id from reviewer where amazon_reviewer_id = \'%s\' limit 1), \
-        (SELECT product_id from product where asin = \'%s\' limit 1), \'%s\', \'%s\', \
-        %d, %d, to_timestamp(\'%s\', \'dd mm yyyy\'));' % (amazon_reviewer_fk, \
-        review_asin, review_text, summary, overall, unix_review_time, review_date))
-        print(cur.statusmessage)
-        if count % 50 == 0:
+    if not exists:
+        val_string += ',(nextval(\'review_serial\'), \
+                        (SELECT reviewer_id from reviewer where amazon_reviewer_id = \'%s\' limit 1), \
+                        (SELECT product_id from product where asin = \'%s\' limit 1), \'%s\', \'%s\', \
+                        %d, %d, to_timestamp(\'%s\', \'dd mm yyyy\'))' % (amazon_reviewer_fk, \
+                        review_asin, review_text, summary, overall, unix_review_time, review_date)
+        reviewer_data.append([amazon_reviewer_fk,unix_review_time])
+        
+        if count % 100 == 0:
+            cur.execute('INSERT INTO REVIEWER (reviewer_id, amazon_reviewer_id, reviewer_name)\
+                        VALUES %s' % reviewer_string[1:])
+            reviewer_string = ''
+            cur.execute('INSERT INTO Review (review_id, reviewer_id, product_id, review_text, \
+                summary, overall, unix_review_time, review_time) VALUES %s' % val_string[1:])
             conn.commit()
             print('committed')
+            print(overall)
+            val_string = ''
 
 #commit and close
 conn.commit()
